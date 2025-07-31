fdd34e70fbe00d8326d38e585215e8ba708be838
Swan☝️🤓 <swansaunglwin276work@gmail.com>
2025-07-31 10:46:27 +0100

    feat: book model added

--- api/models.py
+++ api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+  isbn = models.CharField(max_length=17, primary_key=True) # isbn has 13 digits and 4 hyphens
+  title = models.CharField(max_length=255)
+  description = models.TextField(default="")
+  author = models.CharField(max_length=255)
+  publishedDate = models.DateField() # no validation for future date as there can be pre-orders
