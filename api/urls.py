eb35fba05dfd9400e017418cd40001908c376abc
Swan☝️🤓 <swansaunglwin276work@gmail.com>
2025-07-31 22:50:25 +0100

    feat: get details, put, patch, delete endpoints added

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, re_path
-from .views import book_view, health_view
+from .views import book_view, book_detail_view, health_view
 
 app_name = "api"
 
 urlpatterns = [
     path("", health_view, name="health"),
-    re_path(r"^books/", book_view, name="books")
+    re_path(r"^books/$", book_view, name="books"),
+    re_path(r"^books/(?P<id>[0-9]+)/$", book_detail_view, name="book_detail"),
 ]
--- api/views.py
+++ api/views.py
@@ -20,10 +20,10 @@ class HealthView(APIView):
 health_view = HealthView.as_view()
 
 class BookView(APIView):
-  """CRUD Books"""
+  """Create, Retrieve Books"""
   
   def get(self, request, *args, **kwargs):
-    """Get all books"""
+    """Get all books""" 
     
     books = Book.objects.all()
     serializer = BookSerializer(books, many=True)
@@ -34,11 +34,9 @@ class BookView(APIView):
     
     serializer = BookSerializer(data=request.data)
     if not serializer.is_valid():
-      print("Not Valid")
       errorResponse = {
         "errors": serializer.errors
       }
-      print(errorResponse)
       return Response(errorResponse, status=status.HTTP_400_BAD_REQUEST)
     
     serializer.save()
@@ -46,3 +44,57 @@ class BookView(APIView):
     return Response(serializer.data, status=status.HTTP_201_CREATED)
   
 book_view = BookView.as_view()
+
+class BookDetailView(APIView):
+  """Retrieve, Update, Delete a Book"""
+  
+  def get_book(self, id):
+    try:
+      return Book.objects.get(id=id)
+    except Book.DoesNotExist:
+      raise Http404
+  
+  def get(self, request, id, *args, **kwargs):
+    book = self.get_book(id)
+    serializer = BookSerializer(book)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+  
+  def put(self, request, id, *args, **kwargs):
+    book = self.get_book(id)
+    serializer = BookSerializer(book, data=request.data)
+    
+    if not serializer.is_valid():
+      errorResponse = {
+        "errors": serializer.errors
+      }
+      return Response(errorResponse, status=status.HTTP_400_BAD_REQUEST)
+    
+    serializer.save()
+    
+    return Response(serializer.data, status=status.HTTP_200_OK)
+  
+  def patch(self, request, id, *args, **kwargs):
+    book = self.get_book(id)
+    serializer = BookSerializer(book, data=request.data, partial=True)
+    
+    if not serializer.is_valid():
+      errorResponse = {
+        "errors": serializer.errors
+      }
+      return Response(errorResponse, status=status.HTTP_400_BAD_REQUEST)
+    
+    serializer.save()
+    
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+  def delete(self, request, id, *args, **kwargs):
+    book = self.get_book(id)
+    book.delete()
+    return Response(
+      {
+        "id": id,
+      },
+      status=status.HTTP_200_OK
+    )
+  
+book_detail_view = BookDetailView.as_view()
