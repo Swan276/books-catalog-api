986cb09cdccd939a2bdecd917607b3f72afcc485
Swan☝️🤓 <swansaunglwin276work@gmail.com>
2025-07-31 12:47:31 +0100

    feat: book get all and insert endpoints added

--- api/models.py
+++ api/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
 class Book(models.Model):
-  isbn = models.CharField(max_length=17, primary_key=True) # isbn has 13 digits and 4 hyphens
+  id = models.AutoField(primary_key=True)  
+  isbn = models.CharField(max_length=17, unique=True) # isbn has 13 digits and 4 hyphens
   title = models.CharField(max_length=255)
-  description = models.TextField(default="")
+  description = models.TextField(default="") # default to empty if not provided
   author = models.CharField(max_length=255)
-  publishedDate = models.DateField() # no validation for future date as there can be pre-orders
+  published_date = models.DateField() # no validation for future date as there can be pre-orders
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
